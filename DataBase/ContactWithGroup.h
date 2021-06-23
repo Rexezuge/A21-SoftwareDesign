b0530d0697af970310e5c3c7e90529f04d43959d
Zhenyuan Gong <ZhenyuanGong2001@gmail.com>
2021-06-23 19:37:24 -0400

    DataBase v0.2.4 	modified:   ContactWithGroup.h

--- DataBase/ContactWithGroup.h
+++ DataBase/ContactWithGroup.h
@@ -1,59 +1,49 @@
 #ifndef SDD_EASYCONTACT_DATABASE_CONTACTWITHGROUP
 #define SDD_EASYCONTACT_DATABASE_CONTACTWITHGROUP
 #include<memory>
-#include"ContactNoGroup.h"
+#include"ContactNOGroup.h"
 using namespace std;
 class ContactWithGroup{
-vector<unique_ptr<ContactNoGroup>> Rep;
+vector<unique_ptr<ContactNOGroup>> _Rep;
     public:
-    ContactWithGroup();
-    void addGroup(const string* name);
-    void addContact(const string* groupName,const Contact* contact);
-    void setContact(const Contact* contact);
-    void setPriority(const Contact* contact,int priority);
-    void setPriority(const string* groupName,int priority);
+    ContactWithGroup() { _Rep=vector<unique_ptr<ContactNOGroup>>(); }
+    void addGroup(const string* name) { _Rep.push_back(unique_ptr<ContactNOGroup>(new ContactNOGroup(name))); }
+    void addContact(const string* groupName,const Contact* contact){
+      for(int i=0;i<_Rep.size();i++){
+          if(!_Rep[i].getName().compare(groupName)){
+              _Rep[i].addContact(contact);
+          }
+      }
+    }
+    auto getContact(const string* groupName){
+      for(int i=0;i<_Rep.size();i++){
+        if(!_Rep[i].getName().equals(groupName)){
+          return _Rep[i].getList();
+        }
+      }
+    }
+    void setContact(const Contact* contact){
+      for(int i=0;i<_Rep.size();i++){
+          if(_Rep[i].contains(contact)){
+              _Rep[i].setContact(contact);
+          }
+      }
+    }
+    void setPriority(const Contact* contact,int priority){
+      for(int i=0;i<_Rep.size();i++){
+          if(_Rep[i].contains(contact)){
+              _Rep[i].setPriority(priority);
+          }
+      }
+    }
+    void setPriority(const string* groupName,int priority){
+      for(int i=0;i<_Rep.size();i++){
+          if(!_Rep[i].getName().compare(groupName)){
+              auto tmpBuffer=_Rep[i+priority];
+              _Rep[i+priority]=_Rep[i];
+              _Rep[i]=tmpBuffer;
+          }
+      }
+    }
 };
-
-Contact::ContactWithGroup(){
-    Rep=vector<ContactNoGroup*>();
-}
-
-void Contact::addGroup(const string* name){
-    Rep.push_back(unique_ptr<ContactNoGroup>(new ContactNoGroup));
-}
-
-void Contact::addContact(const string* groupName,const Contact* contact){
-    for(int i=0;i<Rep.size();i++){
-        if(!Rep[i].getName().compare(groupName)){
-            Rep[i].addContact(contact);
-        }
-    }
-}
-
-void Contact::setContact(const Contact* contact){
-    for(int i=0;i<Rep.size();i++){
-        if(Rep[i].contains(contact)){
-            Rep[i].setContact(contact);
-        }
-    }
-}
-
-void setPriority(const Contact* contact,int priority){
-    for(int i=0;i<Rep.size();i++){
-        if(Rep[i].contains(contact)){
-            Rep[i].setPriority(priority);
-        }
-    }
-}
-
-void setPriority(const string* groupName,int priority){
-    for(int i=0;i<Rep.size();i++){
-        if(!Rep[i].getName().compare(groupName)){
-            auto tmpBuffer=Rep[i+priority];
-            Rep[i+priority]=Rep[i];
-            Rep[i]=tmpBuffer;
-        }
-    }
-}
-
 #endif
