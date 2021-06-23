a0811219c6c26b1a6964ff57675a2400980d4200
JensenChen <a1043904820@gmail.com>
2021-06-23 20:30:14 +0800

    Update FileIO.h
    create the readfile func but not have a test

--- DataBase/FileIO.h
+++ DataBase/FileIO.h
@@ -5,7 +5,42 @@
 #include<string>
 class FileIO{
   FILE* FPRep;
+  vector<string> InputInfo;
+  vector<string> primaryInfo;
   public:
-    FileIO(std::string FileName) { FPRep=fopen(FileName.c_str(),"r"); }
+    FileIO(string FileName) { FPRep=fopen(FileName.c_str(),"r"); }
+    std::vector<string> GatherInfoFromFile(){
+      if(FPRep==NULL) perror ("Error opening file");
+      else{
+        int c;
+        do{
+          string InputChar;
+          c=getc(FPRep);
+          InputChar=putchar(c);
+          InputInfo.push_back(InputChar);
+        }while(c!=EOF);
+        fclose(FPRep);
+        int start_index=0;
+        int end_index=0;
+        for(int i=0;i<InputInfo.size();i++)
+        {
+          if(InputInfo[i].compare("\"")&&start_index==0)
+            start_index=i;
+          else if(InputInfo[i].compare("\"")&&start_index!=0)
+            end_index=i;
+          if(start_index!=0&&end_index!=0)
+          {
+            string temp_element=InputInfo[start_index+1];
+            for(int i=start_index+2;i<end_index;i++)
+            {
+              temp_element=temp_element+InputInfo[start_index];
+            }
+            primaryInfo.push_back(temp_element);
+            start_index=0;
+            end_index=0;
+          }
+        }
+      }
+    }
 };
 #endif
