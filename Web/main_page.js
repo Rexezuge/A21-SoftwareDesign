6e05e7ce579f55ab61091549c63797c068bc85ca
JensenChen <a1043904820@gmail.com>
2021-07-08 15:03:06 +0800

    finish the basic front-end

---
+++ Web/main_page.js
@@ -0,0 +1,8 @@
+function displayAddContact() {
+    document.getElementById("Area_Add_Contact").style.display = "inline";
+}
+
+function FakedisplayAddContact() {
+    document.getElementById("Area_Add_Contact").style.display = "none";
+}
+
