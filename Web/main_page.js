7a53f8cf31525b14c3211d79efc406fd67bbc142
JensenChen <a1043904820@gmail.com>
2021-07-21 22:32:46 +0800

    finish connecting to mongoDB

--- Web/main_page.js
+++ Web/main_page.js
@@ -1,13 +1,13 @@
-$(document).ready(function(){
-  	$("button.test").click(function(){
-  	alert("test");
-  	url = '/addMember/NotToday&b';
-    $.get(url, function(data){
-      alert("back");
-    });
-  });
-});
-
+// $(document).ready(function(){
+//   	$("button.test").click(function(){
+//   	alert("test");
+//   	url = '/addMember/NotToday&b';
+//     $.get(url, function(data){
+//       alert("back");
+//     });
+//   });
+// });
+// const axios = require('axios').default;
 function displayAddContact() {
 	document.getElementById("Contact_group").style.display = "none";
 	document.getElementById("Area_Add_Contact_Group").style.display = "none";
@@ -29,4 +29,39 @@ function FakedisplayAddContact() {
 
 function goToAccountPage(){
 	window.location.href='sublink/account.html';
+}
+
+function addContact() {
+	var contact_name = document.querySelector('#add_contact_name').value;
+	var contact_email_1 = document.querySelector('#add_contact_email_1').value;
+	var contact_email_2 = document.querySelector('#add_contact_email_2').value;
+	var contact_group = document.querySelector('#add_contact_group').value;
+	if (contact_name == "" || contact_email_1 == "" || contact_group == "") {
+		alert("Info Missed!")
+	} else {
+		if (contact_email_2 == "") {
+			contact_email_2 = "none"
+		}
+		console.log("Start to post the info!")
+		url = "http://localhost:3000/addContact/" + contact_name + '/' + contact_email_1 + '/' + contact_email_2 + '/' + contact_group
+	}
+		$.post(url, function(data) {
+				console.log(data);
+			}
+		);
+}
+
+function addGroup() {
+	var new_group = document.querySelector('#add_new_group').value;
+	var define_new_group = document.querySelector('#define_new_group').value;
+	if (new_group == "" || define_new_group == "") {
+		alert("Info Missed!")
+	} else {
+		console.log("Start to post the info!")
+		url = "http://localhost:3000/addGroup/" + new_group + '/' + define_new_group
+	}
+		$.post(url, function(data) {
+				console.log(data);
+			}
+		);
 }
--- Web/server.js
+++ Web/server.js
@@ -5,74 +5,149 @@ const path = require('path');
 var axios = require('axios');
 const fetch = require('node-fetch');
 var mongoose = require('mongoose');
+var cors = require('cors');
+app.use(cors())
+
 var url = "mongodb+srv://yef3:rpi123456@cluster0.ro74a.mongodb.net/contactbook";
 
-//Connect to mongodb
-const connection = mongoose.createConnection(url,{ useUnifiedTopology: true, useNewUrlParser: true});
+app.use(express.static(__dirname));
 
+mongoose.connect(url, { useUnifiedTopology: true, useNewUrlParser: true });
+
+// connect to the database and inform
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 db.once('open', function() {
   console.log("MongoDB database connection established successfully");
 });
 
-//Schemas
-const {Schema} = mongoose;
-const GroupSchema = new Schema({
-    groupName: String,
-    members: [
-        {contactName: String,
-        phoneNumber: Number,
-        adress: String}
-    ]
-}, {collection: "groups"})
-const Group= connection.model("groups", GroupSchema, "groups");
 
-app.use(express.static(__dirname));
 app.listen(3000, () => {
     console.log(`ContactBook app listening at http://localhost:3000`)
-  })
-
-
-app.get("/addGroup/:groupname", function(req, res){
-    console.log(req.params.groupname)
-    var text = req.params.groupname
-    Group.findOne({groupName:text}, (err, user)=>{
-    	if (err) throw err;
-	    if (user == null){
-	        var newuser = new Group({
-                groupName: String(text),
-	            members: []
-	        })
-	        newuser.save((err)=>{
-	            if (err) throw err;
-	            console.log("Group " + req.params.username +" added")
-	        })
-	    }
-	    else{
-	        console.log("Already have " + text + " group");
-	        res.status(200);
-    	}
-    })
 })
 
-app.get("/addMember/:username&:groupname", function(req, res){
-    console.log(req.params.username)
-    var text = req.params.username
-    var group = req.params.groupname
-    Group.findOne({groupName:group}, (err, user)=>{
-        if (err) throw err;
-        if (user != null){
-            var newuser = {
-                members:{
-                contactName : String(text),
-                phoneNumber: Number(524252340742),
-                adress: String("noooooo")
-                }
-            }
-            Group.updateOne({groupName:group},{'$push': newuser} ).then(result => {
-  }).catch(err => console.error(`Failed to update the item: ${err}`))
+// Schemas
+const { Schema } = mongoose;
+const contactInfo = new Schema ({
+    account_name: {type: String},
+    mail_address_1: {type: String},
+    mail_address_2: {type: String},
+    group_in: {type: String}
+});
+const ContactInfo = mongoose.model('ContactInfo', contactInfo);
 
+app.post('/addContact/:account_name/:mail_address_1/:mail_address_2/:group_in', function(req, res) {
+    var account_name = req.params.account_name;
+    var mail_address_1 = req.params.mail_address_1;
+    var mail_address_2 = req.params.mail_address_2;
+    var group_in = req.params.group_in;
+
+    // avoid saving repetitive info
+    ContactInfo.findOne({account_name: account_name, group_in: group_in}).exec(function(err, contact) {
+        if (contact) {
+            res.json({'msg': 'Existed'});
+        } else {
+            var thisContactInfo = new ContactInfo({
+                account_name: account_name,
+                mail_address_1: mail_address_1,
+                mail_address_2: mail_address_2,
+                group_in: group_in
+            })
+            thisContactInfo.save(function (err) {
+                if (err) res.json(err);
+                else res.json({'msg': 'Saved'})
+            })
         }
-    })
+    });
 })
+
+const groupInfo = new Schema ({
+    group_name: {type: String},
+    note: {type: String},
+});
+const GroupInfo = mongoose.model('GroupInfo', groupInfo);
+
+app.post('/addGroup/:group_name/:note', function(req, res) {
+    var group_name = req.params.group_name;
+    var note = req.params.note;
+
+    // avoid saving repetitive info
+    GroupInfo.findOne({group_name: group_name, note: note}).exec(function(err, group) {
+        if (group) {
+            res.json({'msg': 'Existed'});
+        } else {
+            var thisGroupInfo = new GroupInfo({
+                group_name: group_name,
+                note: note
+            })
+            thisGroupInfo.save(function (err) {
+                if (err) res.json(err);
+                else res.json({'msg': 'Saved'})
+            })
+        }
+    });
+})
+
+// axios.post('http://localhost:3000/addGroup/Fake_Group/Yes,_it_is_fake', {
+// }).then(response => {
+//   console.log(response.data.msg)
+//   if (response.data.msg == "Saved") {
+//     // document.querySelector('.saved_btn_1').innerHTML = "Saved"
+//     console.log("Saved")
+//   }
+// })
+
+// //Schemas
+// const {Schema} = mongoose;
+// const GroupSchema = new Schema({
+//     groupName: String,
+//     members: [
+//         {contactName: String,
+//         phoneNumber: Number,
+//         adress: String}
+//     ]
+// }, {collection: "groups"})
+// const Group= connection.model("groups", GroupSchema, "groups");
+
+// app.get("/addGroup/:groupname", function(req, res){
+//     console.log(req.params.groupname)
+//     var text = req.params.groupname
+//     Group.findOne({groupName:text}, (err, user)=>{
+//     	if (err) throw err;
+// 	    if (user == null){
+// 	        var newuser = new Group({
+//                 groupName: String(text),
+// 	            members: []
+// 	        })
+// 	        newuser.save((err)=>{
+// 	            if (err) throw err;
+// 	            console.log("Group " + req.params.username +" added")
+// 	        })
+// 	    }
+// 	    else{
+// 	        console.log("Already have " + text + " group");
+// 	        res.status(200);
+//     	}
+//     })
+// })
+
+// app.get("/addMember/:username&:groupname", function(req, res){
+//     console.log(req.params.username)
+//     var text = req.params.username
+//     var group = req.params.groupname
+//     Group.findOne({groupName:group}, (err, user)=>{
+//         if (err) throw err;
+//         if (user != null){
+//             var newuser = {
+//                 members:{
+//                 contactName : String(text),
+//                 phoneNumber: Number(524252340742),
+//                 adress: String("noooooo")
+//                 }
+//             }
+//             Group.updateOne({groupName:group},{'$push': newuser} ).then(result => {
+//   }).catch(err => console.error(`Failed to update the item: ${err}`))
+
+//         }
+//     })
+// })
