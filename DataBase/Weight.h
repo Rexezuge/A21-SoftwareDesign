350617c82d8d412173e15873f8d1f71b27472419
Zhenyuan Gong <ZhenyuanGong2001@gmail.com>
2021-07-21 23:11:07 -0400

    DB Main Version 0.1.6 	modified:   DataBase/Weight.h

--- DataBase/Weight.h
+++ DataBase/Weight.h
@@ -6,7 +6,6 @@ class Weight{
   std::string _Mail;
   std::list<int> _Times;
   public:
-    Weight();
     void newMail(const std::string& newMail,const int MailTime) { _Mail=newMail; _Times.push_front(MailTime); }
     const std::string& getMail() { return _Mail; }
     int getPreferedTime();
