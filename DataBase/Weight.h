c40f9db74bcb96ecb40381d8c121398c9e59db33
Zhenyuan Gong <ZhenyuanGong2001@gmail.com>
2021-08-04 17:03:21 -0400

    Weight > Update Algorithm 	modified:   Weight.h

--- DataBase/Weight.h
+++ DataBase/Weight.h
@@ -12,15 +12,15 @@ class Weight{
     const std::string& getMail() { return _Mail; }
     int getPreferedTime(){
       int _INDEX=0;
-      int _WEIGHT_1=0;
-      int _WEIGHT_2=0;
+      int _WEIGHT_1=0,_WEIGHT_1_SIZE=_Times.size()*0.2;
+      int _WEIGHT_2=0,_WEIGHT_2_SIZE=_Times.size()*0.5;
       int _WEIGHT_3=0;
       for(std::list<int>::iterator i=_Times.begin();i!=_Times.end();i++){
-        if(_INDEX<=_Times.size()*0.2) { _WEIGHT_1+=*i; }
-        if(_INDEX>_Times.size()*0.2&&_INDEX<=_Times.size()*0.5) { _WEIGHT_2+=*i; }
-        if(_INDEX>_Times.size()*0.5) { _WEIGHT_3+=*i; }
+        if(_INDEX<=_WEIGHT_1_SIZE) { _WEIGHT_1+=*i; }
+        if(_INDEX>_WEIGHT_1_SIZE&&_INDEX<=_WEIGHT_2_SIZE) { _WEIGHT_2+=*i; }
+        if(_INDEX>_WEIGHT_2_SIZE) { _WEIGHT_3+=*i; }
         _INDEX++;
       }
-      return _WEIGHT_1*0.4+_WEIGHT_2*0.3+_WEIGHT_3*0.3;
+      return _WEIGHT_1/_WEIGHT_1_SIZE*0.4+_WEIGHT_2/_WEIGHT_2_SIZE*0.3+_WEIGHT_3/_WEIGHT_2_SIZE*0.3;
     }
 };
