7fb46275e3773a6a18f4802bd4a619adbdea8c5b
Henry Hongbin Liu <henryhongbinliu@gmail.com>
2021-08-07 02:29:17 -0400

    Debug

--- DataBase/DB_Import.h
+++ DataBase/DB_Import.h
@@ -2,7 +2,7 @@
 
 class DB_Import{
   string fileName;
-  vector <unique_ptr<ContactNOGroup>> contactBook;
+  ContactWithGroup contactBook;
 
   public:
     //Constructor
@@ -16,60 +16,61 @@ class DB_Import{
         file.open(fileName);
 
         //read file
-        while (file) {
-          //read each line
-          getline(file, line);
+        if(file.is_open()){
+          string line;
+          while (getline(file, line)) {
 
-          //Store contact info
-          string groupName, contactName, mailAddress, phoneNumber;
-          //Store info position
-          int start, end;
-          //Verify which info
-          int check = 0;
+            //Store contact info 
+            string groupName, contactName, mailAddress, phoneNumber;
+            //Store info position
+            int start, end;
+            //Verify which info
+            int check = 0;
 
-          //Run through each character
-          for(int i=0; i< line.length()-1; i++){
-            //get current character
-            char tmp1 = line.at(i);
-            char tmp2 = line.at(i+1);
+            //Run through each character
+            for(unsigned int i=0; i<line.length()-1; i++){
+              //get current character
+              char tmp1 = line[i];
+              char tmp2 = line[i+1];
 
 
-            //If Contact info start
-            if(tmp1=="\"" && tmp2!="," && tmp2!="\n" && start==0){
-              start = i+1;
-            //If Contact info end
-            } else if (tmp1!="\"" && tmp1!="," && tmp2=="\"" && end==0){
-              end = i;
-            }
-            //Locate info
-            if (start!=0 && end!=0){
-              string info = line.substr(start, end);
-              start = 0;
-              end = 0;
+              //If Contact info start
+              if(tmp1=='\"' && tmp2!=',' && tmp2!='\n' && start==0){
+                start = i+1;
+              //If Contact info end
+              } else if (tmp1!='\"' && tmp1!=',' && tmp2=='\"' && end==0){
+                end = i;
+              }
+              //Locate info
+              if (start!=0 && end!=0){
+                string info = line.substr(start, end);
+                start = 0;
+                end = 0;
 
-              //set contact info
-              if(check==0){
-                groupName = info;
-              } else if(check==1){
-                contactName = info;
-              } else if(check==2){
-                mailAddress = info;
-              } else {
-                phoneNumber = info;
-                check = 0;
+                //set contact info
+                if(check==0){
+                  groupName = info;
+                } else if(check==1){
+                  contactName = info;
+                } else if(check==2){
+                  mailAddress = info;
+                } else {
+                  phoneNumber = info;
+                  check = 0;
+                }
               }
             }
-          }
 
-          //compose Contact object
-          Contact newContact = Contact(contactName, atoi(phoneNumber.c_str()), mailAddress);
+            //compose Contact object
+            Contact newContact = Contact(contactName, atoi(phoneNumber.c_str()), mailAddress);
 
-          //If new Contact list
-          if(!contactBook.containBook(groupName)){
-            contactBook.addGroup(groupName);
-            contactBook.addContact(groupName, newContact);
-          } else {
-            contactBook.addContact(groupName, newContact);
+            //If new Contact list
+            if(!contactBook.containBook(groupName)){
+              contactBook.addGroup(groupName);
+              contactBook.addContact(groupName, newContact);
+            } else {
+              contactBook.addContact(groupName, newContact);
+            }
           }
         }
 
