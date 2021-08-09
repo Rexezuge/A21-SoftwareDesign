cffb8102d9b9ef6c67ddc0e83aa899685f3f5fc8
Jerry Lu <lu1342392260@hotmail.com>
2021-08-08 21:19:29 -0400

    cp0

--- DataBase/APIAccess.h
+++ DataBase/APIAccess.h
@@ -1,7 +1,19 @@
 #include"DATABASE_INCLUDE.h"
 
+extern ContactWithGroup* _Rep;
+
 bool addGroup(const std::string& GROUPNAME){
   return _Rep->addGroup(GROUPNAME);
+}
+
+std::vector<std::string> getContact(const std::string& NAME) {
+  Contact tmp = _Rep->getContact(NAME);
+  std::vector<std::string> ret;
+  ret.push_back(tmp.getName());
+  ret.push_back(std::to_string(tmp.getPhone()));
+  ret.push_back(tmp.getAddress());
+
+  return ret;
 }
 
 bool addContact(const std::string& GROUPNAME,const Contact& CONTACT){
@@ -16,8 +28,13 @@ bool removeContact(const std::string& CONTACTNAME){
   return _Rep->removeContact(CONTACTNAME);
 }
 
-std::list<Contact> getGroup(const std::string& GROUPNAME){
-  return _Rep->getGroup(GROUPNAME);
+std::vector<Contact> getGroup(const std::string& GROUPNAME){
+  std::list<Contact> tmp = _Rep->getGroup(GROUPNAME);
+  std::vector<Contact> ret;
+  for (std::list<Contact>::iterator i = tmp.begin(); i != tmp.end(); i++) {
+    ret.push_back(*i);
+  }
+  return ret;
 }
 
 bool editContact(const std::string& OLDCONTACT,const std::string& GROUPNAME,const Contact& CONTACT){
--- DataBase/APIRouter.h
+++ DataBase/APIRouter.h
@@ -1,29 +1,87 @@
-#include"DATABASE_INCLUDE.h"
+#ifndef SDD_EASYCONTACT_API_ROUTER
+#define SDD_EASYCONTACT_API_ROUTER
+
+#include <string>
+
+#include "APIAccess.h"
+#include "Contact.h"
+#include "libhv/HttpService.h"
 
 class APIRouter {
     public:
     static void register_router(HttpService& router) {
-        router.GET("/contacts", [](HttpRequest* req, HttpResponse* resp) {
+        router.GET("/contacts/:groupName/:contactName", [](HttpRequest* req, HttpResponse* resp) {
+            std::string groupName = req->GetParam("groupName");
+            std::string contactName = req->GetParam("contactName");
+
+            if (groupName == "" || contactName == "")  return 400;
+
+            resp->content_type = APPLICATION_JSON;
+
+            if (groupName == "NULL") {
+
+            } else if (contactName == "NULL") {
+                std::list<Contact> tmp = getGroup(groupName);
+
+            } else {
+                std::vector<std::string> q = getContact(contactName);
+                if (q[0] == "NULL")  return 404;
+                resp->json["name"] = q[0];
+                resp->json["phone"] = q[1];
+                resp->json["email"] = q[2];
+                resp->json["mostRecent"];
+                resp->json["received"];
+            }
+
             return 200;
         });
-        router.POST("/contacts", [](HttpRequest* req, HttpResponse* resp) {
+        router.POST("/contacts/:groupName/:contactName", [](HttpRequest* req, HttpResponse* resp) {
+            std::string groupName = req->GetParam("groupName");
+            std::string contactName = req->GetParam("contactName");
             return 201;
         });
-        router.PUT("/contacts", [](HttpRequest* req, HttpResponse* resp) {
+        router.PUT("/contacts/:groupName/:contactName", [](HttpRequest* req, HttpResponse* resp) {
+            std::string groupName = req->GetParam("groupName");
+            std::string contactName = req->GetParam("contactName");
             return 200;
         });
 
-        router.GET("/groups", [](HttpRequest* req, HttpResponse* resp) {
+        router.GET("/groups/:groupName/:contactName/:newGroupName", [](HttpRequest* req, HttpResponse* resp) {
+            std::string groupName = req->GetParam("groupName");
+            resp->content_type = APPLICATION_JSON;
+            std::vector<std::string> groups = getGroups();
+            for (int i = 0; i < groups.size(); i++) {
+                std::string group = groups[i];
+                resp->json.push_back(group);
+            }
             return 200;
         });
-        router.POST("/groups", [](HttpRequest* req, HttpResponse* resp) {
+        router.POST("/groups/:groupName/:contactName/:newGroupName", [](HttpRequest* req, HttpResponse* resp) {
+            if (req->content_type != APPLICATION_JSON)  return 400;
+            std::string groupName = req->GetParam("groupName");
+
+            std::string topContact = req->GetString("topContact");
+            std::string topGroup = req->GetString("topGroup");
+
+            if (groupName == "")       return 400;
+            if (!addGroup(groupName))  return 410;
+
+
+
             return 201;
         });
-        router.PUT("/groups", [](HttpRequest* req, HttpResponse* resp) {
+        router.PUT("/groups/:groupName/:contactName/:newGroupName", [](HttpRequest* req, HttpResponse* resp) {
+            std::string groupName = req->GetParam("groupName");
+
             return 200;
         });
-        router.PATCH("/groups", [](HttpRequest* req, HttpResponse* resp) {
+        router.PATCH("/groups/:groupName/:contactName/:newGroupName", [](HttpRequest* req, HttpResponse* resp) {
+            std::string groupName = req->GetParam("groupName");
+            std::string contactName = req->GetParam("contactName");
+            std::string newGroupName = req->GetParam("newGroupName");
             return 200;
         });
     }
 };
+
+#endif
