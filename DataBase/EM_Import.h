c6e7ccd8defdf561c61876f9b7db534181accca3
Zhenyuan Gong <ZhenyuanGong2001@gmail.com>
2021-08-11 20:44:01 -0400

    EmailImport > Update 	modified:   EM_Import.h 	new file:   newMail.txt

--- DataBase/EM_Import.h
+++ DataBase/EM_Import.h
@@ -3,6 +3,10 @@
 bool Import_Email_From_Local(const char* FileName,ContactWithGroup* DB){
   BYPASSUNUSED(DB);
   FILE* FD=fopen(FileName,"r");
+  char _CONTEXT[4096];
+  while(!strncmp(fgets(_CONTEXT,4096,FD),"s",1)){
+    BYPASSUNUSED(_CONTEXT);
+  }
   fclose(FD);
   return EXIT_SUCCESS;
 }
