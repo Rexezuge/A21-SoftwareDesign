72dfccface04bce7ab22e173a59090b0acf77cf3
yef3 <yef3@rpi.edu>
2021-08-11 20:30:14 -0400

    Add top button
    Add top button after every group. Add top and delete button beside every contact

--- Web/main_page.js
+++ Web/main_page.js
@@ -94,6 +94,8 @@ function showOneGroupContact(group_name){
 				var mail1 = data[i].mail_address_1;
 				var mail2 = data[i].mail_address_2;
 				var contactHtml = '<li class="Contact">\
+					            <button class="Delete_Contact">Delete</button>\
+								<button class="Top_Contact">Top</button>\
 					            <p class="Contact_Name">'+contact_name+'</p>\
 					            <p class="Address">'+mail1+'</p>\
 					            <p class="Address">'+mail2+'</p>\
@@ -115,7 +117,7 @@ function shwoAllGroups(){
 			length = data.length;
 			for (i=0;i<length;i++){
 				group = data[i].group_name;
-				groupHtml = '<a class="Contact_Group" >'+group+' </a><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right" viewBox="1 -3 16 16"><path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"/> </svg>'
+				groupHtml = '<a class="Contact_Group" >'+group+' </a><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right" viewBox="1 -3 16 16"><path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"/></svg><button class="Top_Button">Top</button>'
 				$("#groups_block").append(groupHtml);
 			}
 			//Show the first group automatically
