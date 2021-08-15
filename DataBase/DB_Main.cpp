3360fa957e0db1fabe5a8ce907065d476bc4c605
Henry Hongbin Liu <henryhongbinliu@gmail.com>
2021-08-14 20:27:26 -0400

    1

--- DataBase/DB_Main.cpp
+++ DataBase/DB_Main.cpp
@@ -76,7 +76,7 @@ int main(int numArgs,char** Argv){
   printf("==EZCT== Easy Contact BackEnd DataBase is Now Running...\n");
   printf("==EZCT== To Terminate, Press \"Ctrl+C\"\n");
   _Rep=(ContactWithGroup*)malloc(sizeof(ContactWithGroup));
-  *_Rep=DB_Import("ECDB.csv").extract();
+  _Rep=DB_Import("ECDB.csv").extract();
   // Import_Email_From_Local("ECDB_EM.csv",_Rep);
   pthread_create(&PID_ER,0,StartEmailReader,_Rep);
   pthread_create(&PID_PR,0,StartPrioritySort,_Rep);
