42b7a4a907e0a89af60bc3a88dc2b584851e27d8
Henry Hongbin Liu <henryhongbinliu@gmail.com>
2021-08-14 22:01:57 -0400

    2.0

--- DataBase/EM_Import.h
+++ DataBase/EM_Import.h
@@ -10,8 +10,10 @@ bool Import_Email_From_Local(const char* FileName,ContactWithGroup* DB){
   if(file.is_open()){
     string line;
     while (getline(file, line)) {
-      string input1, input2, input3, input4;
 
+      //Store contact info
+      string input1, input2, input3, input4;
+      //Store info position
       int start=0;
       int end=0;
       //Verify which info
@@ -23,12 +25,11 @@ bool Import_Email_From_Local(const char* FileName,ContactWithGroup* DB){
         char tmp1 = line[i];
         char tmp2 = line[i+1];
 
-        //If Contact info start
         if(tmp1=='\"' && tmp2!=',' && tmp2!='\n' && start==0){
           start = i+1;
         //If Contact info end
-        } else if (tmp1!='\"' && tmp1!=',' && tmp2=='\"' && end==0){
-          end = i;
+        } else if (tmp1=='\"' && (tmp2==',' || tmp2=='\n') && end==0){
+          end = i-start;
         }
         //Locate info
         if (start!=0 && end!=0){
@@ -36,27 +37,25 @@ bool Import_Email_From_Local(const char* FileName,ContactWithGroup* DB){
           start = 0;
           end = 0;
 
-          //set contact info
           if(check==0){
             input1 = info;
+            check++;
           } else if(check==1){
             input2 = info;
+            check++;
           } else if(check==2){
             input3 = info;
-          } else {
+            check++;
+          } else if(check==3){
             input4 = info;
             check = 0;
+            DB->restoreWeight(input1, input2, input3, input4);
           }
         }
       }
-
-      DB->restoreWeight(input1, input2, input3, input4);
     }
   }
 
-
-
   file.close();
-
-  return EXIT_SUCCESS;
+  return true;
 }
