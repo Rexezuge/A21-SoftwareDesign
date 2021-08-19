8a28b232eda48304e66b96287684621c0e5daeeb
Viyerelu23333 <11363469+Viyerelu23333@users.noreply.github.com>
2021-08-19 17:32:58 -0400

    feat: multithread compile

--- INSTALL.sh
+++ INSTALL.sh
@@ -1,3 +1,5 @@
+thds=$(cat /proc/cpuinfo | grep "processor" | wc -l)
+echo "Will compile with ${thds} thread(s)"
 echo "Internet Connection & Super User Permission is Required!"
 echo "This Installer is Design for Ubuntu & Tested on Ubuntu 20.04 LTS"
 echo "Now Insatlling Pre-Requisite..."
@@ -9,7 +11,7 @@ echo ""
 echo "Now Installing Easy Contact..."
 git submodule update --init --recursive
 cmake .
-cmake --build .
+cmake --build . -j $thds
 cd Web
 npm install
 cd ..
