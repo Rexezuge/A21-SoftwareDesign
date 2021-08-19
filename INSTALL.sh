912e227353f2e43df703a72328e60d8cba4ffddd
Zhenyuan Gong <79224316+reC4P7CH4@users.noreply.github.com>
2021-08-19 15:14:29 -0400

    Create INSTALL.sh

---
+++ INSTALL.sh
@@ -0,0 +1,8 @@
+echo("Internet Connection & Super User Permission is Required!")
+echo("This Installer is Design for Ubuntu & Tested on Ubuntu 20.04 LTS")
+echo("Now Insatlling Pre-Requisite...")
+sudo apt update
+sudo apt install g++ cmake
+echo("Pre-Requisite Installation Complete")
+echo("")
+echo("")
