8489c86d3d626e9c0b5f19ae428dc18809732761
Zhenyuan Gong <79224316+reC4P7CH4@users.noreply.github.com>
2021-08-19 15:25:23 -0400

    Update INSTALL.sh

--- INSTALL.sh
+++ INSTALL.sh
@@ -1,8 +1,17 @@
-echo("Internet Connection & Super User Permission is Required!")
-echo("This Installer is Design for Ubuntu & Tested on Ubuntu 20.04 LTS")
-echo("Now Insatlling Pre-Requisite...")
+echo "Internet Connection & Super User Permission is Required!"
+echo "This Installer is Design for Ubuntu & Tested on Ubuntu 20.04 LTS"
+echo "Now Insatlling Pre-Requisite..."
 sudo apt update
-sudo apt install g++ cmake
-echo("Pre-Requisite Installation Complete")
-echo("")
-echo("")
+sudo apt install g++ cmake nodejs
+echo "Pre-Requisite Installation Complete"
+echo ""
+echo ""
+echo "Now Installing Easy Contact..."
+git submodule update --init --recursive
+cmake .
+cmake --build .
+cd Web
+npm install
+cd ..
+cd DataBase && ./ecdb &
+cd Web && node server.js
